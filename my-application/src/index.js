1c344c5dbeb789ffe2214fef43dd3c8b5d382bde
senthuran16 <senthuranrc16@gmail.com>
2017-09-06 15:36:38 +0530

    Implemented with Dummy methods instead of API Calls

--- my-application/src/index.js
+++ my-application/src/index.js
@@ -10,8 +10,8 @@ class BusinessRules extends React.Component {
         }
     }
 
-    // Renders available TemplateGroups as thumbnails
     render() {
+        // Display available Template Groups as thumbnails
         const templateGroups = this.state.templateGroups.map((templateGroup) =>
             <TemplateGroup
                 key={templateGroup.name}
@@ -24,13 +24,15 @@ class BusinessRules extends React.Component {
 
         return (
             <div>
-                <h2>Template Groups</h2>
-                <div>{templateGroups}</div>
+                {templateGroups}
             </div>
         );
     }
 }
 
+/**
+ * Represents a Template Group
+ */
 class TemplateGroup extends React.Component {
     constructor(props) {
         super(props);
@@ -38,33 +40,24 @@ class TemplateGroup extends React.Component {
             name: props.name,
             description: props.description,
             ruleTemplates: props.ruleTemplates,
-            // 'thumbnail' when displaying available TemplateGroups
-            // 'parent' when displaying available RuleTemplates under this
+            // "thumbnail" when displaying available Template Groups
+            // "parent" when displaying available Rule Templates under this
             viewAs: props.viewAs
         }
     }
 
-    // Handles Click on thumbnail
-    loadRuleTemplates() {
-        //load("ruleTemplates", this, this.state.name)
-        // Get RuleTemplates of this TemplateGroup
-        var ruleTemplates = getRuleTemplates(this.state.name)
-        load("ruleTemplates", this, this.state.name)
-    }
-
-    // Renders each available TemplateGroup as thumbnail, or
-    // RuleTemplates under a given TemplateGroup as thumbnails (TemplateGroup as parent)
     render() {
-        // View available TemplateGroups as Thumbnails
+        // View Template Group as a thumbnail
         if (this.state.viewAs === "thumbnail") {
             return (
-                <div className="templateGroupCard" onClick={(e) => this.loadRuleTemplates(e)}>
+                <div className="templateGroupCard" onClick={(e) => displayRuleTemplates(this.state.name)}>
                     <h3>{this.state.name}</h3>
                     <p>{this.state.description}</p>
                 </div>
             );
         } else {
-            // View RuleTemplates, available under this TemplateGroup
+            // View each Rule Template under this Template Group, as a thumbnail
+            // todo: map and return Rule Templates under this
             const ruleTemplates = this.state.ruleTemplates.map((ruleTemplate) =>
                 <RuleTemplate
                     key={ruleTemplate.name}
@@ -77,21 +70,18 @@ class TemplateGroup extends React.Component {
                     templates={ruleTemplate.templates}
                     properties={ruleTemplate.properties}
                     viewAs="thumbnail"
-                />
-            );
-
+                />);
             return (
                 <div>
-                    <h2>Rule Templates</h2>
-                    <p>{this.state.name}</p>
-                    <div>{ruleTemplates}</div>
-                </div>
-            );
+                    {ruleTemplates}
+                </div>);
         }
     }
-
 }
 
+/**
+ * Represents a Rule Template
+ */
 class RuleTemplate extends React.Component {
     constructor(props) {
         super(props);
@@ -104,72 +94,26 @@ class RuleTemplate extends React.Component {
             description: props.description,
             templates: props.templates,
             properties: props.properties,
-            // 'thumbnail' when displaying available RuleTemplates
-            // 'form' when filling the values for Templates
+            // "thumbnail" when displaying available Template Groups
+            // "parent" when displaying available Rule Templates under this
             viewAs: props.viewAs
         }
     }
 
-    // Button Click
-    loadRuleTemplateForm() {
-        load("ruleTemplateForm", this, this.state.name) //todo: replace 'some' with proper one
-    }
-
-    // Renders properties of the RuleTemplate as a form, or
-    // each RuleTemplate as a thumbnail
     render() {
-        // View as a form
-        if (this.state.viewAs === "form") {
-            // Convert objects, to an objects array
-            var propertiesArray = []
-            for (var propertyKey in this.state.properties) {
-                var propertyKeyString = propertyKey.toString()
-                // Push as an object,
-                // that has the original object's Key & Value
-                // denoted by new Keys : 'propertyName' & 'propertyObject'
-                propertiesArray.push(
-                    {
-                        propertyName: propertyKey,
-                        propertyObject: this.state.properties[propertyKeyString]
-                    }
-                )
-            }
-
-            const properties = propertiesArray.map((property) =>
-                <Property
-                    key={property.propertyName}
-                    name={property.propertyName}
-                    description={property.propertyObject.description}
-                    defaultValue={property.propertyObject.defaultValue}
-                    type={property.propertyObject.type}
-                    options={property.propertyObject.options}
-                />
-            );
-
+        if (this.state.viewAs === "thumbnail") {
             return (
-                <div>
-                    <h3>Property values for Template</h3>
-                    <p>
-                        {this.state.templateGroupName} > {this.state.name}
-                    </p>
-                    <form>
-                        {properties}<br/>
-                        <input type="submit" value="Submit"/>
-                    </form>
-                </div>
-            );
-        } else {
-            // View as thumbnail
-            return (
-                <div className="ruleTemplateCard" onClick={(e) => this.loadRuleTemplateForm(e)}>
+                <div className="ruleTemplateCard"
+                     onClick={(e) =>
+                         displayForm(
+                             this.state.templateGroupName,
+                             this.state.name,
+                             this.state.properties)}>
                     <h3>{this.state.name}</h3>
+                    <p>{this.state.description}</p>
                     <p>
-                        {this.state.description}
-                        <br/>
-                        <br/>
-                        Type : {this.state.type}
-                        <br/>
-                        Instance Count : {this.state.instanceCount}
+                        Type : {this.state.type}<br/>
+                        Instance Count : {this.state.instanceCount}<br/>
                     </p>
                 </div>
             );
@@ -177,38 +121,88 @@ class RuleTemplate extends React.Component {
     }
 }
 
+class BusinessRuleForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            templateGroupName: props.templateGroupName,
+            ruleTemplateName: props.ruleTemplateName,
+            properties: props.properties
+        }
+    }
+
+    render() {
+        // Convert objects, to an objects array
+        var propertiesArray = []
+        for (var propertyKey in this.state.properties) {
+            var propertyKeyString = propertyKey.toString()
+            // Push as an object,
+            // which has the original object's Key & Value
+            // denoted by new Keys : 'propertyName' & 'propertyObject
+            propertiesArray.push(
+                {
+                    propertyName: propertyKey,
+                    propertyObject: this.state.properties[propertyKeyString]
+                }
+            )
+        }
+        const properties = propertiesArray.map((property) =>
+            <Property
+                key={property.propertyName}
+                name={property.propertyName}
+                description={property.propertyObject.description}
+                defaultValue={property.propertyObject.defaultValue}
+                type={property.propertyObject.type}
+                options={property.propertyObject.options}
+            />
+        );
+        return (
+            <div>
+                <h3>Business Rule</h3>
+                <h4>{this.state.templateGroupName} > {this.state.ruleTemplateName}</h4>
+                <br/>
+                <form>
+                    {properties}<br/>
+                    <input type="submit" value="Submit"/>
+                </form>
+            </div>
+        );
+    }
+
+}
+
+/**
+ * Represents a Property
+ */
 class Property extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            // Templated element with passed props
-            property: {
-                name: props.name,
-                description: props.description,
-                defaultValue: props.defaultValue,
-                type: props.type,
-                options: props.options
-            }
+            name: props.name,
+            description: props.description,
+            defaultValue: props.defaultValue,
+            type: props.type,
+            options: props.options
         }
     }
 
     // Renders each element either as a TextField or Select box
     render() {
         var htmlSource = "";
-        htmlSource += this.state.property.description + " : <br/>";
-        if (this.state.property.type === "options") {
+        htmlSource += this.state.description + " : <br/>";
+        if (this.state.type === "options") {
             // Options : List view
-            htmlSource += "<select name=" + this.state.property.name + " value=" + this.state.property.defaultValue + ">";
+            htmlSource += "<select name=" + this.state.name + " value=" + this.state.defaultValue + ">";
 
             // Each given option
-            for (let option of this.state.property.options) {
+            for (let option of this.state.options) {
                 htmlSource += "<option value=" + option + ">" + option + "</option>";
             }
 
             htmlSource += "</select>";
         } else {
             // Text Field
-            htmlSource += "<input type=text name=" + this.state.property.name + " value=" + this.state.property.defaultValue + " required >";
+            htmlSource += "<input type=text name=" + this.state.name + " value=" + this.state.defaultValue + " required >";
         }
         htmlSource += "<br/><br/>"
         return (
@@ -216,25 +210,29 @@ class Property extends React.Component {
         );
     }
 
-    /**
-     * Converts a given string to HTML
-     * @param inputString Given string
-     * @returns {{__html: *}} Converted HTML
-     */
     toHTML(inputString) {
         return {
             __html: inputString
         }
     }
-
 }
 
 /**
- * Gets available TemplateGroups from the API and returns them todo: implement with API
- * @returns {{templateGroups: [null,null]}} JSON array, containing TemplateGroups
+ * Starts BusinessRules
+ */
+function startBusinessRules() {
+    // Recieve TemplateGroups from API
+    var receivedTemplateGroups = getTemplateGroups()
+    // ReactDOM.render(<BusinessRules
+    //     templateGroups={receivedTemplateGroups.templateGroups}/>, document.getElementById("root"));
+}
+
+/**
+ * Get available TemplateGroups todo: from API
+ * @returns {Array}
  */
 function getTemplateGroups() {
-    var receivedTemplateGroups = {
+    var receivedTemplateGroups = { // todo: from API
         templateGroups: [
             {
                 "name": "SensorDataAnalysis1",
@@ -413,105 +411,64 @@ function getTemplateGroups() {
         ]
     }
 
-    return receivedTemplateGroups
+    return receivedTemplateGroups.templateGroups
 }
 
 /**
- * Gives RuleTemplates, available under given TemplateGroup todo: implement with API
+ * Get availanle RuleTemplates from given TemplateGroup todo: from API
  * @param templateGroupName
- * @returns {*|Array}
  */
-function getRuleTemplates(templateGroupName){
-    var templateGroups = getTemplateGroups().templateGroups
-    for(var templateGroup in templateGroups){
-        if(templateGroup.name === templateGroupName){
+function getRuleTemplates(templateGroupName) {
+    var templateGroups = getTemplateGroups()
+    console.log(templateGroups)
+    for (let templateGroup of templateGroups) { // todo: from API
+        if (templateGroup.name === templateGroupName) {
+            console.log("Returned RuleTemplates : ")
+            console.log(templateGroup.ruleTemplates)
             return templateGroup.ruleTemplates
         }
     }
 }
 
-/**
- * Gives Properties of a given RuleTemplate, which belongs to a given TemplateGroup
- * @param templateGroupName
- * @param ruleTemplateName
- * @returns {*|Array}
- */
-function getProperties(templateGroupName,ruleTemplateName){
-    var ruleTemplates = getRuleTemplates(templateGroupName);
-    for(var ruleTemplate in ruleTemplates){
-        if(ruleTemplate.name === ruleTemplateName){
-            return ruleTemplate.properties
-        }
-    }
+function run() {
+    console.log("[Started Business Rules]")
+    var availableTemplateGroups = getTemplateGroups()
+    console.log("Available Template Groups : ")
+    console.log(availableTemplateGroups)
+
+    // Pass available Template Groups into BusinessRules
+    displayTemplateGroups(availableTemplateGroups)
 }
 
-/**
- * Renders an element / a list of elements, as specified
- *
- * @param elementTypeToLoad Type of element (templateGroups, ruleTemplates, ruleTemplateForm)
- * @param parent The object, from which the elementTypeToLoad's details are loaded
- * @param parentName Name of the parent
- */
-function load(elementTypeToLoad, content, parentName) {
-    if (elementTypeToLoad === "templateGroups") {
-        /**
-         * content : Contains array of TemplateGroup Objects
-         * parentName : null
-         */
-        ReactDOM.render(<BusinessRules templateGroups={content.templateGroups}/>, document.getElementById("root"));
-    } else if (elementTypeToLoad === "ruleTemplates") {
-        // content is the TemplateGroup Object
-        /**
-         * content : The TemplateGroup object, that contains array of RuleTemplate objects
-         * parentName : null (extracted from the TemplateGroup object) todo: haven't done. Do
-         */
-        ReactDOM.render(
-            <TemplateGroup
-                name={content.state.name}
-                description={content.state.description}
-                ruleTemplates={content.state.ruleTemplates}
-                viewAs="parent"/>, document.getElementById("root"));
-    } else if (elementTypeToLoad === "ruleTemplateForm") {
-        // content is the RuleTemplate Object
-        /**
-         * content : The RuleTemplate object, of which the properties are going to be loaded
-         * parentName : TemplateGroup object's name
-         */
-        ReactDOM.render(
-            <RuleTemplate
-                templateGroupName={content.state.templateGroupName}
-                name={content.state.name}
-                type={content.state.type}
-                instanceCount={content.state.instanceCount}
-                script={content.state.script}
-                description={content.state.description}
-                templates={content.state.templates}
-                properties={content.state.properties}
-                viewAs="form"/>, document.getElementById("root"));
-    }
+function displayTemplateGroups(availableTemplateGroups) {
+    ReactDOM.render(
+        <BusinessRules
+            templateGroups={availableTemplateGroups}
+        />, document.getElementById("root"))
 }
 
-// Renders given Template Groups
-function loadTemplateGroups(templateGroups){
- const mappedTemplateGroups = templateGroups.map((templateGroup) =>
-     <TemplateGroup
-         key={templateGroup.name}
-         name={templateGroup.name}
-         description={templateGroup.description}
-         ruleTemplates={templateGroup.ruleTemplates}
-         viewAs="thumbnail"
-     />
- );
+function displayRuleTemplates(templateGroupName) {
+    ReactDOM.render(
+        <TemplateGroup
+            key={templateGroupName}
+            name={templateGroupName}
+            ruleTemplates={getRuleTemplates(templateGroupName)}
+            viewAs="parent"
+        />, document.getElementById("root")
+    )
 }
 
-/**
- * Starts BusinessRules
- */
-function startBusinessRules() {
-    // Recieve TemplateGroups from API
-    var receivedTemplateGroups = getTemplateGroups()
-    ReactDOM.render(<BusinessRules
-        templateGroups={receivedTemplateGroups.templateGroups}/>, document.getElementById("root"));
+function displayForm(templateGroupName, ruleTemplateName, properties) {
+    // console.log("Displaying Form")
+    // console.log("RuleTemplate : "+ruleTemplateName)
+    // console.log("Properties : ")
+    // console.log(properties)
+    ReactDOM.render(
+        <BusinessRuleForm
+            templateGroupName={templateGroupName}
+            ruleTemplateName={ruleTemplateName}
+            properties={properties}
+        />, document.getElementById("root"));
 }
 
-startBusinessRules();
+run();
