7420c608b507368c8444167598ccc40bdedbc1ff
senthuran16 <senthuranrc16@gmail.com>
2017-10-11 11:12:07 +0530

    # Stringified JSON objects that is to be sent to the API

--- my-application/src/components/BusinessRuleFromTemplateForm.js
+++ my-application/src/components/BusinessRuleFromTemplateForm.js
@@ -89,7 +89,7 @@ class BusinessRuleFromTemplateForm extends React.Component {
 
         // Send prepared business rule object to API
         let apis = new BusinessRulesAPIs(BusinessRulesConstants.APIS_URL)
-        apis.createBusinessRule(businessRuleObject).then(function(response){
+        apis.createBusinessRule(JSON.stringify(businessRuleObject)).then(function(response){
             console.log("RESPONSE")
             console.log(response)
         })
